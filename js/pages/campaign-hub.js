56d8e9d77c8a921b6cf922c6608bf0d15f63780a
Thales Janisch Santos <thales3006@gmail.com>
2026-06-04 18:40:30 -0300

    feat: UI delete button for campaign-hub

--- js/pages/campaign-hub.js
+++ js/pages/campaign-hub.js
@@ -6,16 +6,33 @@ const campaign_list = document.querySelector("#campaign-list");
 
 const campaign_repository = new CampaignRepository();
 
+function remove_campaign(id) {
+    campaign_repository.delete_campaign(id);
+    const campaign_card = document.querySelector(`#campaign-${id}`);
+    campaign_card.remove();
+}
+
 function CampaignCardComponent(campaign) {
-  const div = document.createElement('div');
-  div.className = 'campaign-card';
-  
-  div.innerHTML = `
-    <h2>${campaign.title}</h2>
-    <p>${campaign.description}</p>
-  `;
-  
-  return div;
+    const div = document.createElement('div');
+    div.className = 'campaign-card';
+    div.id = `campaign-${campaign.id}`;
+
+    const btn_delete = document.createElement("button");
+    btn_delete.type = "button";
+    btn_delete.textContent = "Delete";
+
+    btn_delete.addEventListener("click", () => {
+        remove_campaign(campaign.id);
+    });
+
+    div.innerHTML = `
+        <h2>${campaign.title}</h2>
+        <p>${campaign.description}</p>
+    `;
+
+    div.appendChild(btn_delete);
+
+    return div;
 }
 
 function regenerate_view_campaigns() {
@@ -33,10 +50,8 @@ async function load_campaigns() {
 btn_add_campaign.addEventListener("click", () => {
     const campaign = new Campaign(null, "title", "description");
 
-    campaign_repository.add_campaign(campaign);
+    campaign_repository.create_campaign(campaign);
     campaign_list.append(CampaignCardComponent(campaign));
-
-    console.log(campaign_repository.campaigns);
 });
 
 load_campaigns();
