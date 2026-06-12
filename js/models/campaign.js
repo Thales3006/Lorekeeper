944ba6c1da063bd8fa4adc09de02b04b366df596
Thales Janisch Santos <thales3006@gmail.com>
2026-06-12 18:07:47 -0300

    feat: added player IDs in campaigns and created at date fields

--- js/models/campaign.js
+++ js/models/campaign.js
@@ -1,7 +1,9 @@
 export class Campaign {
-    constructor(id, title, description) {
+    constructor(id, title, description, players_id = [], created_at = Date.now()) {
         this.id = id;
         this.title = title;
         this.description = description;
+        this.players_id = players_id;
+        this.created_at = created_at;
     }
 }
